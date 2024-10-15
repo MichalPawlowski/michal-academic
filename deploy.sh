f2ced2696b7ccbd29d647e37e39dbac338090140
MichalPawlowski <37312041+MichalPawlowski@users.noreply.github.com>
2024-10-15 16:02:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Remove existing public folder if it exists
+rm -rf public
+
+# Clone the repository's `gh-pages` branch into `public`
+git clone --branch gh-pages "https://${GITHUB_TOKEN}@github.com/MichalPawlowski/michal-academic.git" public
+
+# Build the site into the public folder
+hugo -d public
+
+# Navigate to the public folder
+cd public
 
 # Configure Git
 git config --global user.email "mp371303@students.mimuw.edu.pl"
 git config --global user.name "MichalPawlowski"
 
-# Clone the repository's `gh-pages` branch into `public`
-git clone --branch gh-pages "https://${GITHUB_TOKEN}@github.com/MichalPawlowski/michal-academic.git" public
-
-# Copy the built files to the `gh-pages` branch
-rsync -av --delete public/ public/
-
-cd public
-a
 # Add and commit changes
 git add .
 git commit -m "Deploy at $(date '+%Y-%m-%d %H:%M:%S')"
 
-# Push changes to `gh-pages` branch
+# Push to the `gh-pages` branch
 git push origin gh-pages
